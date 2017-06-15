796774a7c515f01604caa0f12df4344313ee99fc
Lukáš Frýdek <ahoj@lukasfrydek.cz>
2017-06-15 12:12:58 +0200

    Adding BASE_URL

--- index.php
+++ index.php
@@ -20,16 +20,20 @@
             `'-.   .-'`
                 \ /
 */
-
+// Environment
 define('DS', DIRECTORY_SEPARATOR);
 define('ENV', getenv('APP_ENV'));
 define('VERSION', '0.12.1');
 define('MIGRATION_NUMBER', 220);
 
+// Paths
 define('PATH', __DIR__ . DS);
 define('APP', PATH . 'anchor' . DS);
 define('SYS', PATH . 'system' . DS);
 define('EXT', '.php');
+
+// Urls
+define('BASE_URL', dirname($_SERVER['SCRIPT_NAME']));
 
 require APP . 'composer_check' . EXT;
 require SYS . 'start' . EXT;
